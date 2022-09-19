627130ea2c2cbdd43afff5cc170a15718ca62641
Wilmar Vitor Fonseca <wilmarfonseca@gec.inatel.br>
2022-09-19 20:39:30 -0300

    Limpando tarefas antigas

--- 02_pythonEPoo/model/pessoa.py
+++
@@ -1,8 +0,0 @@
-class Pessoa():
-    def __init__(self,nome,idade,sexo):
-        self.nome = nome
-        self.idade = idade
-        self.sexo = sexo
-    
-    def envelhecer(self):
-        self.idade += 1
